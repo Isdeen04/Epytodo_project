575fd67ad47f217d28e156a7da820dee255181a7
Isdeen <isdeen.mora-yerima@epitech.eu>
2023-12-06 11:24:19 +0100

    Creation of a new user registration route in the auth.js route authentication file.

---
+++ src/routes/auth/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { createUser, getUserByEmail, comparePasswords } = require('../user/user.query');
+const { verifyToken } = require('../middleware/auth');
+
+router.post('/register', async (req, res) => {
+  const { email, password, name, firstname } = req.body;
+
+  try {
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ msg: 'Account already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await createUser(email, hashedPassword, name, firstname);
+
+    const token = jwt.sign({ id: newUser.id }, process.env.SECRET, { expiresIn: '1h' });
+
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(500).json({ msg: 'Internal Server Error' });
+  }
+});
