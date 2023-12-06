cf5ffc380aaffc144f34c0d1f188784dce3fb1f5
Isdeen <isdeen.mora-yerima@epitech.eu>
2023-12-06 11:51:04 +0100

    Retrieve specific tasks by ID and create new tasks in the todo.js file.

--- src/routes/todos/todo.js
+++ src/routes/todos/todo.js
@@ -19,3 +19,26 @@ router.get('/todos', async (req, res) => {
     res.status(500).json({ msg: 'Internal Server Error' });
   }
 });
+
+router.get('/todos/:id', async (req, res) => {
+  const todoId = req.params.id;
+  try {
+    const todo = await getTodoById(todoId);
+    if (!todo) {
+      return res.status(404).json({ msg: 'Not found' });
+    }
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ msg: 'Internal Server Error' });
+  }
+});
+
+router.post('/todos', async (req, res) => {
+  const { title, description, due_time, status } = req.body;
+  try {
+    const newTodo = await createTodo(title, description, due_time, status, req.user.id);
+    res.status(201).json(newTodo);
+  } catch (error) {
+    res.status(500).json({ msg: 'Internal Server Error' });
+  }
+});
