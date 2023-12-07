e72b371468b70af6112d8f9ab4df70969964fc34
Isdeen <isdeen.mora-yerima@epitech.eu>
2023-12-07 10:08:26 +0100

    Added a function that updates user information in the file that executes SQL queries related to user authentication.

--- src/routes/user/auth.query.js
+++ src/routes/user/auth.query.js
@@ -11,3 +11,18 @@ async function createUser(email, password, name, firstname) {
   const [result] = await db.query(query, [email, password, name, firstname]);
   return { id: result.insertId, email, password, name, firstname };
 }
+
+async function updateUser(id, email, password, name, firstname) {
+  const query = 'UPDATE user SET email = ?, password = ?, name = ?, firstname = ? WHERE id = ?';
+  const [result] = await db.query(query, [email, password, name, firstname, id]);
+  if (result.affectedRows > 0) {
+    return { id, email, password, name, firstname };
+  }
+  return null;
+}
+
+module.exports = {
+  getUserByEmail,
+  createUser,
+  updateUser
+};
