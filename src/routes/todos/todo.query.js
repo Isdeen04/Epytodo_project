366ef7522d33ee44d01ceb401133938d8ed1002e
Isdeen <isdeen.mora-yerima@epitech.eu>
2023-12-07 09:39:46 +0100

    added a function to retrieve specific tasks by ID in the SQL query execution file todos.query.js

--- src/routes/todos/todo.query.js
+++ src/routes/todos/todo.query.js
@@ -5,3 +5,9 @@ async function getAllTodos(userId) {
   const [todos] = await db.query(query, [userId]);
   return todos;
 }
+
+async function getTodoById(todoId) {
+  const query = 'SELECT * FROM todo WHERE id = ?';
+  const [todo] = await db.query(query, [todoId]);
+  return todo[0];
+}
